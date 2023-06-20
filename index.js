141f9d5a1c11129654cbc8644744e4bfd50b0ba3
jiwookim7211 <108187456+jiwookim7211@users.noreply.github.com>
2023-06-20 10:27:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ level: 3 // 지도의 확대 레벨
 };
 										
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption); 
+var map = new kakao.maps.Map(mapContainer, mapOption);
 var markerPosition = new kakao.maps.LatLng(35.9460546, 126.6821658);
 var marker = new kakao.maps.Marker({
 position: markerPosition
